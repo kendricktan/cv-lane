bd3d298775ccad408b54e4e9cc3464d1a45507d4
kendricktan <kendricktan0814@gmail.com>
2016-04-10 21:23:24 +1000

    added kalman filter

--- cv-lane.py
+++ cv-lane.py
@@ -1,5 +1,6 @@
 from cv.EyeCanSee import *
 from ai.pid import *
+from ai.KalmanFilter import *
 from controller.controllers import *
 
 import cv2
@@ -13,7 +14,13 @@ camera = EyeCanSee()
 # PID for each region (if we do decide to add any)
 pid = {}
 for region in settings.REGIONS_KEYS:
-    pid[region] = PID(p=.35, i=.125, d=.305, integrator_max=10, integrator_min=-10)
+    pid[region] = PID(p=.35, i=.125, d=.075, integrator_max=50, integrator_min=-50)
+
+# Kalman filter
+measurement_standard_deviation = 0
+process_variance = 15
+estimated_measurement_variance = measurement_standard_deviation ** 2  # 0.05 ** 2
+kalman_filter = KalmanFilter(process_variance, estimated_measurement_variance)
 
 motor = MotorController() # values: -100 <= x <= 100
 steering = ServoController() # values: 0 <= x <= 100
@@ -27,7 +34,10 @@ for i in range(0, 125):
     # Pid on each region
     total_pid = 0
     for region in camera.relative_error:
-        total_pid += pid[region].update(camera.relative_error[region])
+        kalman_filter.input_latest_noisy_measurement(camera.relative_error[region])
+        filtered_value = kalman_filter.get_latest_estimated_measurement()
+        #print(filtered_value)
+        total_pid += pid[region].update(filtered_value)
     # Negative total_pid = need to turn left
     # Positive total_pid = need to turn right
     # Try to keep pid 0
